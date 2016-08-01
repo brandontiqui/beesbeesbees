a3bf06b6165750786dd254e1ea3d23734873695c
Brandon Tiqui <btiqui@gmail.com>
2016-08-01 11:48:07 -0700

    Create Bee class in pseudoclassical style

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
 var Bee = function() {
+	Grub.call(this); // get attributes
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = 'keep on growing';
 };
+Bee.prototype = Object.create(Grub.prototype); // get methods
+Bee.prototype.constructor = Bee; // restore constructor
 
